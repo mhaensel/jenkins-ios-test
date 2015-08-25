ca54e71c3407e2485f5d2e0f34f30bda58eb5792
Matthias Hänsel <matthias.haensel@enthusiastudios.com>
2015-08-25 13:40:42 +0200

    new xcode project

---
+++ JenkinsIOSTest/JenkinsIOSTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JenkinsIOSTest
+//
+//  Created by Matthias Hänsel on 24/08/15.
+//  Copyright (c) 2015 Matthias Hänsel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
